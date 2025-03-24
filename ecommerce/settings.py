9eab70473022db8647d40bfebd0a9220d524486e
anamolbasnet50 <anamolbasnet50@gmail.com>
2025-03-24 20:07:59 +0545

    Fix homepage issue

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,8 +155,9 @@ TEMPLATES_DIR = BASE_DIR / "templates"
 TEMPLATES[0]["DIRS"] = [TEMPLATES_DIR]
 
 # Static & Media Files
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
