f63c20866faf69c45cd9b3f7d0e354909792fd96
anamolbasnet50 <anamolbasnet50@gmail.com>
2025-03-25 16:46:19 +0545

    Added auto superuser creation

--- manage.py
+++ manage.py
@@ -11,12 +11,7 @@ django.setup()
 
 User = get_user_model()
 if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
-    User.objects.create_superuser(
-        username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
-        email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
-        password=os.getenv("DJANGO_SUPERUSER_PASSWORD"),
-    )
-    print("Superuser created successfully!")
+    
 
 
 def main():
