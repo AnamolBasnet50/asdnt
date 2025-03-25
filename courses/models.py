cc54dcca73715b282cead00a0bf4c21202014da4
anamolbasnet50 <anamolbasnet50@gmail.com>
2025-03-25 17:13:13 +0545

    Fixed media file issues

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-
 class Course(models.Model):
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = cloudinary.models.CloudinaryField('image')  # Store images in Cloudinary
+    instructor = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
