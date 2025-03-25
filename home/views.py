46df60c8ef73ba97f41ca8af4a93104f444732e1
anamolbasnet50 <anamolbasnet50@gmail.com>
2025-03-25 07:07:05 +0545

    Updated home page with Bootstrap

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from products.models import Product  # Assuming you have a Product model
 
 def home(request):
-    return render(request, "home/index.html")
+    products = Product.objects.all()[:3]  # Show 3 featured products
+    return render(request, "index.html", {"products": products})
